fe2d57c56a386fd1a5a2e87b1a321f5282b1487e
antonio <antonio.stankovic77@gmail.com>
2022-04-11 11:51:50 +0200

    first commit, banner outline

---
+++ src/components/Banner.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="banner">
+    <img src="https://picsum.photos/800/600" alt="banner" />
+    <div class="text">
+      <h1>Welcome to Stronghold of faith</h1>
+      <h2>
+        All the basics of sacred faith on one place, learn it by the heart,
+        click what you want to learn:
+      </h2>
+      <div class="buttons">
+        <button>Sacred Mass</button>
+        <button>Catholic Faith</button>
+        <button>Watch Lessions from Youtube</button>
+        <button>Beautiful Chants and songs</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.banner {
+  display: grid;
+  height: 100vh;
+  /* overflow: hidden; */
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  justify-items: stretch;
+  align-items: stretch;
+}
+img {
+  grid-row: 1 / -1;
+  grid-column: 1/ -1;
+  object-fit: fill;
+  max-height: 100vh;
+}
+.text {
+  position: relative;
+  grid-row: 1 / -1;
+  grid-column: 1 / -1;
+  justify-self: center;
+  display: grid;
+  justify-items: center;
+  align-content: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import Banner from "../components/Banner.vue";
+</script>
+<template>
+  <Banner />
+</template>
